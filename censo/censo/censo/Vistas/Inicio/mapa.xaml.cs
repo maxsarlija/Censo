0eff2a1b997605300495bc3d558e0cd28ed99955
Maximiliano Sarlija <max.sarlija@outlook.com.ar>
2018-01-08 17:47:16 -0300

    vistas

---
+++ censo/censo/censo/Vistas/Inicio/mapa.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace censo.Vistas.Inicio
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class mapa : ContentPage
+	{
+		public mapa ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
